47ca24454bd95c527a8fb185fe7cf9eeab334dfe
Cirrus83 <cyc10@outlook.com>
2025-11-08 09:15:10 +0800

    Add files via upload

--- HeadFiles.h
+++ HeadFiles.h
@@ -3,6 +3,8 @@
 #include"HeadFiles/02_gcd_with_manual.h"
 #include"HeadFiles/02_gpaRescuer.h"
 #include"HeadFiles/02_ipv6prob.h"
+#include"HeadFiles/02_jigai.h"
 #include"HeadFiles/02_kurikaesu.h"
+#include"HeadFiles/02_pkuSchools.h"
 #include"HeadFiles/02_printHelp.h"
 #include"HeadFiles/02_printVer.h"
